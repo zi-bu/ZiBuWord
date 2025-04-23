3e26509f2c05ea8b62465ae7687ad258e21acf7b
zibu <3441479120@qq.com>
2025-04-23 23:28:10 +0800

    添加登录和注册界面元素（人话：加了几个按钮）
    在 `Form1.Designer.cs` 文件中，添加了两个按钮（`button1` 和 `button2`）以及两个文本框（`textBox1` 和 `textBox2`），并设置了它们的位置、名称、大小、标签和其他属性。调整了窗体的大小和边距，并在窗体加载时添加了事件处理程序。
    
    在 `Form1.cs` 文件中，添加了 `button1_Click` 方法的空实现，用于处理 `button1` 的点击事件。

--- 单词背诵程序/UI/Form1.Designer.cs
+++ 单词背诵程序/UI/Form1.Designer.cs
@@ -28,19 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(158, 430);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 58);
+            button1.TabIndex = 0;
+            button1.Text = "登录";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(436, 430);
+            button2.Name = "button2";
+            button2.Size = new Size(114, 58);
+            button2.TabIndex = 1;
+            button2.Text = "注册";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(158, 325);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(392, 23);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "密码";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(158, 253);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(392, 23);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "用户名";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(944, 681);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Margin = new Padding(2);
             Name = "Form1";
+            Padding = new Padding(10);
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
