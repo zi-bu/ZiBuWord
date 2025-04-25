c0a577e0a5d4ffe61bce906f4b6ac6d28a477e3f
Gushiina <1513727898@qq.com>
2025-04-25 16:46:55 +0800

    重排

--- 单词背诵程序/UI/Form1.Designer.cs
+++ 单词背诵程序/UI/Form1.Designer.cs
@@ -37,9 +37,9 @@
             // 
             // button1
             // 
-            button1.Location = new Point(158, 430);
+            button1.Location = new Point(158, 592);
             button1.Name = "button1";
-            button1.Size = new Size(114, 58);
+            button1.Size = new Size(392, 58);
             button1.TabIndex = 0;
             button1.Text = "登录";
             button1.UseVisualStyleBackColor = true;
@@ -47,17 +47,17 @@
             // 
             // button2
             // 
-            button2.Location = new Point(471, 607);
+            button2.Location = new Point(158, 657);
             button2.Margin = new Padding(5, 4, 5, 4);
             button2.Name = "button2";
-            button2.Size = new Size(114, 58);
+            button2.Size = new Size(392, 58);
             button2.TabIndex = 1;
             button2.Text = "注册";
             button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(158, 325);
+            textBox1.Location = new Point(158, 539);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(392, 30);
             textBox1.TabIndex = 2;
@@ -65,39 +65,18 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(158, 253);
+            textBox2.Location = new Point(158, 489);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(392, 30);
             textBox2.TabIndex = 3;
             textBox2.Text = "用户名";
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.ForeColor = Color.DarkSlateBlue;
-            label1.Location = new Point(329, 870);
-            label1.Name = "label1";
-            label1.Size = new Size(82, 24);
-            label1.TabIndex = 4;
-            label1.Text = "游客登陆";
-            label1.Click += label1_Click;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.favicon_3_;
-            pictureBox1.Location = new Point(238, 158);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(259, 250);
-            pictureBox1.TabIndex = 5;
-            pictureBox1.TabStop = false;
-            pictureBox1.Click += pictureBox1_Click_2;
-            // 
             // button3
             // 
-            button3.Location = new Point(683, 607);
+            button3.Location = new Point(158, 792);
             button3.Margin = new Padding(5, 4, 5, 4);
             button3.Name = "button3";
-            button3.Size = new Size(179, 82);
+            button3.Size = new Size(392, 55);
             button3.TabIndex = 4;
             button3.Text = "游客登录";
             button3.UseVisualStyleBackColor = true;
@@ -107,7 +86,7 @@
             // 
             AutoScaleDimensions = new SizeF(11F, 24F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1483, 961);
+            ClientSize = new Size(738, 961);
             Controls.Add(button3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -117,7 +96,6 @@
             Padding = new Padding(16, 14, 16, 14);
             Text = "Form1";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -129,5 +107,7 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Button button3;
+        private Label? label1;
+        private PictureBox pictureBox1;
     }
 }
