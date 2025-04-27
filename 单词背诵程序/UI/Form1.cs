5753b8285a1cf256f2b24c1e9c249f93cec35886
lxhxl <2701433890@qq.com>
2025-04-27 21:49:42 +0800

    重构UI界面，新增登录和主界面功能。Form1.cs中添加了用户名和密码输入框、登录及游客登录按钮，并实现相应事件处理。Form2.cs中新增了主菜单、单词显示面板及控制按钮，优化了界面样式。

--- 单词背诵程序/UI/Form1.cs
+++ 单词背诵程序/UI/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using BLL;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace UI
 {
@@ -10,16 +12,127 @@ namespace UI
     /// </summary>
     public partial class Form1 : Form
     {
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Button btnVisitor;
+        private Label lblTitle;
+        private PictureBox picLogo;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeCustomComponents();
+            ApplyStyling();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // 设置窗体属性
+            this.Text = "单词背诵程序";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            // 标题标签
+            lblTitle = new Label
+            {
+                Text = "欢迎使用单词背诵程序",
+                Font = new Font("微软雅黑", 24, FontStyle.Bold),
+                AutoSize = true,
+                Location = new Point(200, 50)
+            };
+
+            // 用户名输入框
+            txtUsername = new TextBox
+            {
+                PlaceholderText = "请输入用户名",
+                Size = new Size(300, 30),
+                Location = new Point(250, 150),
+                Font = new Font("微软雅黑", 12)
+            };
+
+            // 密码输入框
+            txtPassword = new TextBox
+            {
+                PlaceholderText = "请输入密码",
+                Size = new Size(300, 30),
+                Location = new Point(250, 200),
+                Font = new Font("微软雅黑", 12),
+                PasswordChar = '*'
+            };
+
+            // 登录按钮
+            btnLogin = new Button
+            {
+                Text = "登录",
+                Size = new Size(120, 40),
+                Location = new Point(250, 250),
+                Font = new Font("微软雅黑", 12)
+            };
+            btnLogin.Click += BtnLogin_Click;
+
+            // 游客登录按钮
+            btnVisitor = new Button
+            {
+                Text = "游客登录",
+                Size = new Size(120, 40),
+                Location = new Point(430, 250),
+                Font = new Font("微软雅黑", 12)
+            };
+            btnVisitor.Click += VisitorLogin;
+
+            // 添加控件到窗体
+            this.Controls.AddRange(new Control[] { lblTitle, txtUsername, txtPassword, btnLogin, btnVisitor });
+        }
+
+        private void ApplyStyling()
+        {
+            // 设置背景色
+            this.BackColor = Color.White;
+
+            // 设置按钮样式
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.BackColor = Color.FromArgb(0, 120, 215);
+                    button.ForeColor = Color.White;
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderSize = 0;
+                    button.Cursor = Cursors.Hand;
+                }
+            }
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsername.Text) || string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("请输入用户名和密码！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // TODO: 调用BLL层的登录验证
+            // 如果验证成功，打开主窗体
+            Form2 mainForm = new Form2();
+            mainForm.Show();
+            this.Hide();
+        }
+
+        private void VisitorLogin(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("真的要游客登录吗？您的学习进度将不会被保存。", "提示",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Form2 mainForm = new Form2();
+                mainForm.Show();
+                this.Hide();
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-        private void button1_Click(object sender, EventArgs e)
         {
 
         }
@@ -42,24 +155,6 @@ namespace UI
         private void pictureBox1_Click_2(object sender, EventArgs e)
         {
             MessageBox.Show("恭喜你发现了彩蛋，关注子布喵喵喵");
-        }
-
-        /// <summary>
-        /// 这是游客登录按钮的点击事件处理函数。
-        /// <br/>这里由```子布```编写。
-        /// 之后这段代码可能会被改写成一个函数（放入逻辑层），或者直接删除。
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void VisitorLogin(object sender, EventArgs e)
-        {
-            if(MessageBox.Show("真的要游客登录吗，你所有的数据将不会保存", " 警告！"
-                , MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
-            {
-                Form2 f2 = new Form2();//创建一个新的Form2对象
-                f2.Show();//显示Form2窗口
-                this.Hide();//隐藏当前窗口
-            }
         }
     }
 }
