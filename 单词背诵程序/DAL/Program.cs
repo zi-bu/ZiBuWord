0216cdb4c9d58f19b0bf902ca8bf5352e3ea1d02
Gushiina <1513727898@qq.com>
2025-04-27 21:44:50 +0800

    咕：在DAL层定义关于单词对象的接口范式， 关于实际在BLL调用的方式 请参考文件 单词存储列表的实现设想 第一版。

--- 单词背诵程序/DAL/Program.cs
+++ 单词背诵程序/DAL/Program.cs
@@ -19,84 +19,120 @@ namespace DAL
         {
             List<string> list = new List<string>();
             list = OldSqlOperation.ReadSqlData("CET4-顺序");
-            //foreach (var item in list)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            Console.WriteLine(list[1][1]);
-        }
-    }
-    ///<summary>
-    /// 连接数据库与读取数据
-    /// 这里代码是``子布``用来测试数据库连接的。
-    /// 这里直接使用了原生ADO.NET的SqlClient来连接数据库。<br/>
-    /// 适用场景：需要极致性能、完全控制底层细节，或需要兼容旧代码。<br/>
-    ///优势：<br/>
-    ///直接通过DbConnection、DbCommand等类操作数据库。<br/>
-    ///无额外依赖，适合小型项目或工具类程序。<br/>
-    ///需要手动管理连接和命令对象。<br/>
-    ///准备弃用
-    ///</summary>
-    public static class OldSqlOperation
-    {
-        /// <summary>
-        /// 用于连接数据库的字符串。<br/>
-        /// Server=10.151.196.28,1433;这是在寝室用有线网时的服务器地址。<br/>
-        /// Server=10.162.28.183,1433;这是在自习教室用无线网时的服务器地址。
-        /// </summary>
-        public static string connectionString = "Server=10.162.28.183,1433; Database=背单词; User Id=sa; Password=114514; TrustServerCertificate=True;";
-
-        /// <summary>
-        /// 创建一个SqlConnection对象
-        ///<br/>并连接数据库。
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        public static SqlConnection SqlConnection(string name)
-        {
-            //禁用了SSL证书验证（注意！）
-            SqlConnection connection = new SqlConnection(connectionString);
-            try
+            foreach (var item in list)
             {
-                connection.Open();
-                Console.WriteLine("连接成功！");
+                Console.WriteLine(item);
             }
-            catch (SqlException ex)
-            {
-                Console.WriteLine("连接失败：" + ex.Message);
-            }
-            if (connection.State == System.Data.ConnectionState.Open)
-            {
-                Console.WriteLine("连接已打开！");
-            }
-            else
-            {
-                Console.WriteLine("连接未成功打开！");
-            }
-            return connection;
         }
 
-        /// <summary>
-        /// 读取数据库中的数据<br/>
-        /// "name"是表名
+
+
+        ///<summary>
+        ///此处为咕所写的关于BLL层规范单词对象的接口范式
+        ///可以随意修改，此为一种设想
         /// </summary>
-        /// <param name="name"></param>
-        public static List<string> ReadSqlData(string name)
+        public interface IWord
         {
-            List<string> list = new List<string>();
-            SqlConnection connection = SqlConnection(name);
-            //禁用了SSL证书验证（注意！）
-            SqlCommand command = new SqlCommand($"SELECT * FROM [dbo].[{name}] ORDER BY LEFT(word, 1) ASC;", connection);
-            SqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+            //作为一个单词对象应被实现的字段包含以下  
+            string word { get; } //单词  
+            string pos { get; } //Part of Speech词性  
+            string translation { get; } //释意  
+            string phrase { get; } //短语  
+            string example { get; } //例句  
+
+            //定义一个单词的索引器  
+            string this[int index] { get; }
+            //希望单词能运用下标访问的语法 就是从0到4 一一对应返回上述的内容
+            //而在上层BLL的时候，则会新建一个WordList列表 来存放单词对象 从而实现 WordList[0][1]的单词访问 
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ///<summary>
+        /// 连接数据库与读取数据
+        /// 这里代码是``子布``用来测试数据库连接的。
+        /// 这里直接使用了原生ADO.NET的SqlClient来连接数据库。<br/>
+        /// 适用场景：需要极致性能、完全控制底层细节，或需要兼容旧代码。<br/>
+        ///优势：<br/>
+        ///直接通过DbConnection、DbCommand等类操作数据库。<br/>
+        ///无额外依赖，适合小型项目或工具类程序。<br/>
+        ///需要手动管理连接和命令对象。<br/>
+        ///准备弃用
+        ///</summary>
+        public static class OldSqlOperation
+        {
+            /// <summary>
+            /// 用于连接数据库的字符串。<br/>
+            /// Server=10.151.196.28,1433;这是在寝室用有线网时的服务器地址。<br/>
+            /// Server=10.162.28.183,1433;这是在自习教室用无线网时的服务器地址。
+            /// </summary>
+            public static string connectionString = "Server=10.162.28.183,1433; Database=背单词; User Id=sa; Password=114514; TrustServerCertificate=True;";
+
+            /// <summary>
+            /// 创建一个SqlConnection对象
+            ///<br/>并连接数据库。
+            /// </summary>
+            /// <param name="name"></param>
+            /// <returns></returns>
+            public static SqlConnection SqlConnection(string name)
             {
-                list.Add(reader["word"].ToString());
-                list.Add(reader["translations"].ToString());
-                list.Add(reader["phrase"].ToString());
+                //禁用了SSL证书验证（注意！）
+                SqlConnection connection = new SqlConnection(connectionString);
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("连接成功！");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("连接失败：" + ex.Message);
+                }
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    Console.WriteLine("连接已打开！");
+                }
+                else
+                {
+                    Console.WriteLine("连接未成功打开！");
+                }
+                return connection;
             }
-            //将数据存入list中
-            reader.Close();
-            return list;
+
+            /// <summary>
+            /// 读取数据库中的数据<br/>
+            /// "name"是表名
+            /// </summary>
+            /// <param name="name"></param>
+            public static List<string> ReadSqlData(string name)
+            {
+                List<string> list = new List<string>();
+                SqlConnection connection = SqlConnection(name);
+                //禁用了SSL证书验证（注意！）
+                SqlCommand command = new SqlCommand($"SELECT * FROM [dbo].[{name}] ORDER BY LEFT(word, 1) ASC;", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["word"].ToString() + " " + reader["translations"].ToString());
+                    list.Add(reader["word"].ToString());
+                    list.Add(reader["translations"].ToString());
+                    if (reader["phrase"] != null)
+                        list.Add(reader["phrase"].ToString());
+                }
+                //将数据存入list中
+                reader.Close();
+                return list;
+            }
         }
     }
 }
