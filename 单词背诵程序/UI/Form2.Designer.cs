a30d32124b86244394c1b11d7326f9109024b3ac
nanhe-7 <1046030056@qq.com>
2025-04-28 22:17:49 +0800

    新建form

--- 单词背诵程序/UI/Form2.Designer.cs
+++ 单词背诵程序/UI/Form2.Designer.cs
@@ -34,11 +34,13 @@
             button4 = new Button();
             button5 = new Button();
             button6 = new Button();
+            button7 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(187, 337);
+            button1.Location = new Point(153, 281);
+            button1.Margin = new Padding(2, 2, 2, 2);
             button1.Name = "button1";
             button1.Size = new Size(0, 0);
             button1.TabIndex = 0;
@@ -47,40 +49,40 @@
             // 
             // button2
             // 
-            button2.Location = new Point(90, 106);
-            button2.Margin = new Padding(5, 4, 5, 4);
+            button2.Location = new Point(174, 103);
+            button2.Margin = new Padding(4, 3, 4, 3);
             button2.Name = "button2";
-            button2.Size = new Size(179, 82);
+            button2.Size = new Size(146, 68);
             button2.TabIndex = 1;
-            button2.Text = "开始背诵";
+            button2.Text = "开始记忆";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(90, 270);
-            button3.Margin = new Padding(5, 4, 5, 4);
+            button3.Location = new Point(459, 103);
+            button3.Margin = new Padding(4, 3, 4, 3);
             button3.Name = "button3";
-            button3.Size = new Size(179, 82);
+            button3.Size = new Size(146, 68);
             button3.TabIndex = 2;
             button3.Text = "开始复习";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(569, 106);
-            button4.Margin = new Padding(5, 4, 5, 4);
+            button4.Location = new Point(174, 234);
+            button4.Margin = new Padding(4, 3, 4, 3);
             button4.Name = "button4";
-            button4.Size = new Size(179, 82);
+            button4.Size = new Size(146, 68);
             button4.TabIndex = 3;
             button4.Text = "选择词典";
             button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            button5.Location = new Point(90, 432);
-            button5.Margin = new Padding(5, 4, 5, 4);
+            button5.Location = new Point(489, 391);
+            button5.Margin = new Padding(4, 3, 4, 3);
             button5.Name = "button5";
-            button5.Size = new Size(179, 82);
+            button5.Size = new Size(86, 29);
             button5.TabIndex = 4;
             button5.Text = "退出背词";
             button5.UseVisualStyleBackColor = true;
@@ -88,25 +90,36 @@
             // 
             // button6
             // 
-            button6.Location = new Point(569, 255);
-            button6.Margin = new Padding(5, 4, 5, 4);
+            button6.Location = new Point(459, 234);
+            button6.Margin = new Padding(4, 3, 4, 3);
             button6.Name = "button6";
-            button6.Size = new Size(179, 82);
+            button6.Size = new Size(146, 68);
             button6.TabIndex = 5;
             button6.Text = "单词收藏";
             button6.UseVisualStyleBackColor = true;
             // 
+            // button7
+            // 
+            button7.Location = new Point(187, 391);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 6;
+            button7.Text = "退出登录";
+            button7.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(938, 664);
+            ClientSize = new Size(767, 553);
+            Controls.Add(button7);
             Controls.Add(button6);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form2";
             Text = "Form2";
             Load += Form2_Load;
@@ -121,5 +134,6 @@
         private Button button4;
         private Button button5;
         private Button button6;
+        private Button button7;
     }
 }
---
+++ 单词背诵程序/UI/Form3.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace UI
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            label2 = new Label();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(80, 242);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "认识";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(388, 242);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "不认识";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(101, 104);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(352, 34);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "词义";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(111, 46);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(551, 342);
+            panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(253, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 20);
+            label1.TabIndex = 4;
+            label1.Text = "单词";
+            label1.Click += label1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(101, 156);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(352, 52);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "例句";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(229, 242);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 6;
+            button3.Text = "收藏";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(55, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 7;
+            label2.Text = "词库";
+            label2.Click += label2_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(475, 28);
+            button4.Name = "button4";
+            button4.Size = new Size(50, 29);
+            button4.TabIndex = 8;
+            button4.Text = "首页";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form3";
+            Text = "Form3";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Label label1;
+        private Button button4;
+        private Label label2;
+        private Button button3;
+        private TextBox textBox2;
+    }
+}
