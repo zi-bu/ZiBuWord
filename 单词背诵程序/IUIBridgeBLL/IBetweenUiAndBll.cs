09b32bdf6134226cc44600c3b0c9ef92569dd5fd
zibu <3441479120@qq.com>
2025-05-06 17:18:03 +0800

    修复引用间存在的问题

--- 单词背诵程序/IUIBridgeBLL/IBetweenUiAndBll.cs
+++ 单词背诵程序/IUIBridgeBLL/IBetweenUiAndBll.cs
@@ -1,17 +1,22 @@
-﻿using GlobalIterface;
+﻿
 
 namespace IUIBridgeBLL
 {
     public interface IBetweenUiAndBll
     {
-        void InitializeList(List<IWord> tempWordList);
-       
+        //void InitializeList(List<IWord> tempWordList);存在问题
+
         //What should InitializeList do?
         //1. Initialize the list of words.
         //2. Set correct answer.
         //3. Replace the text of label.
+        /// <summary>
+        /// 组长阅：
+        /// 1.都是中国人说洋文干嘛。<br/>
+        /// 2.都三层架构了，搞个全局的你要干什么？<br/>
+        /// </summary>
 
-        
+
         void ReleaseList();
         
         //What should ReleaseList do?
