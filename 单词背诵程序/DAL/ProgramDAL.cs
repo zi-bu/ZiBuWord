99b12c8280cae4ecff3416d7d8a8b232e1db2186
zibu <3441479120@qq.com>
2025-05-06 16:54:22 +0800

    我跟你们爆了

--- 单词背诵程序/DAL/ProgramDAL.cs
+++ 单词背诵程序/DAL/ProgramDAL.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 //注意这里引用的是Microsoft.Data.SqlClient而不是System.Data.SqlClient(高版本.NET框架已经迁移到Microsoft.Data.SqlClient)
 namespace DAL
 {
@@ -15,22 +14,14 @@ namespace DAL
         {
             using (var db = new SqlDataContext())
             {
-                var datas = db.托福.ToList(); // 从 托福？CET4（√） 表中读取所有数据。
+                var datas = db.托福.ToList(); // 从 CET4 表中读取所有数据。
                 foreach (var s in datas) // 遍历数据并输出到控制台。
                 {
                     try
                     {
-                        string fixedJson = s.phrases
-                        .Replace("'", "\"") // 替换单引号为双引号
-                        .Replace(",}", "}") // 移除多余的逗号
-                        .Replace(",]", "]"); // 移除多余的逗
+                        // 将 phrases 字段的 JSON 转换为 List<Dictionary<string, object>>
+                        var phrasesList = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(s.phrases);
 
-                        var options = new JsonSerializerOptions
-                        {
-                            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull // 忽略空值
-                        };
-
-                        var phrasesList = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(fixedJson, options);
                         Console.WriteLine($"Word: {s.word}");
                         foreach (var phrase in phrasesList)
                         {
