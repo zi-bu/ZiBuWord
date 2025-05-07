d6c5a3627c93f7174a1e9e8232b335d7671b6832
鼠标 <2767002082@qq.com>
2025-05-07 17:09:55 +0800

    优化实体类

--- 单词背诵程序/DAL/dictionary.cs
+++ 单词背诵程序/DAL/dictionary.cs
@@ -12,7 +12,7 @@ namespace DAL
     /// 这个类对应数据库中的表的结构，包含单词、释义和短语等字段。<br/>
     /// 因为一个实体类只能映射一个数据库表
     /// </summary>
-    public abstract class dictionary
+    public  class dictionary
     {
         [Key] // 主键
         public int number { get; set; } // 单词编号
@@ -22,58 +22,10 @@ namespace DAL
         public string? phrases { get; set; } // 短语
     }
 
-    public class CET4 : dictionary
-    {
-        [Key] 
-        public int number { get; set; } 
-        [Required] 
-        public string? word { get; set; } 
-        public string? translations { get; set; } 
-        public string? phrases { get; set; } 
-    }
-    public class CET6 : dictionary
-    {
-        [Key]
-        public int number { get; set; }
-        [Required]
-        public string? word { get; set; } 
-        public string? translations { get; set; } 
-        public string? phrases { get; set; } 
-    }
-    public class 初中 : dictionary
-    {
-        [Key] 
-        public int number { get; set; } 
-        [Required] 
-        public string? word { get; set; } 
-        public string? translations { get; set; } 
-        public string? phrases { get; set; }
-    }
-    public class 高中 : dictionary
-    {
-        [Key]
-        public int number { get; set; } 
-        [Required] 
-        public string? word { get; set; } 
-        public string? translations { get; set; }
-        public string? phrases { get; set; } 
-    }
-    public class 考研 : dictionary
-    {
-        [Key] 
-        public int number { get; set; }
-        [Required] 
-        public string? word { get; set; } 
-        public string? translations { get; set; }
-        public string? phrases { get; set; }
-    }
-    public class 托福 : dictionary
-    {
-        [Key] 
-        public int number { get; set; }
-        [Required]
-        public string? word { get; set; } 
-        public string? translations { get; set; }
-        public string? phrases { get; set; } 
-    }
+    public class CET4 : dictionary { }
+    public class CET6 : dictionary { }
+    public class 初中 : dictionary { }
+    public class 高中 : dictionary { }
+    public class 考研 : dictionary { }
+    public class 托福 : dictionary { }
 }
