ff95e32a4382d9b48a024590c25eaf9e9141e407
Gushiina <1513727898@qq.com>
2025-05-11 20:07:39 +0800

    删除原BLL构层 提供了BLL与DAL层中间的接口设计

---
+++ 单词背诵程序/IBLLBridgeDAL/UserManagement/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace IBLLBridgeDAL.UserManagement;
+
+public interface IUserService//用户服务接口
+{
+    void CreateUser(string username, string password);//创建一个新用户
+    bool InspectUser(string username);//检测一个用户是否存在
+    bool InspectUser(string username, string password);//检测一个已经存在的用户密码是否正确
+}
---
+++ 单词背诵程序/IBLLBridgeDAL/WordOperation/IWordManagement.cs
@@ -0,0 +1,8 @@
+﻿namespace IBLLBridgeDAL.WordOperation;
+
+public interface IWordManagement
+{
+    IWord GetWord();
+    void AddWordToReview(IWord word);
+    void RemoveWordFromReview(IWord word);
+}
