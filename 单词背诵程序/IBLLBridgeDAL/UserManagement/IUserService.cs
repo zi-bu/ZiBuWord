247d756721032e6ff56993dbb8e46caa45d819d2
Gushiina <1513727898@qq.com>
2025-05-11 20:38:50 +0800

    改组接口

--- 单词背诵程序/IBLLBridgeDAL/UserManagement/IUserService.cs
+++ 单词背诵程序/IBLLBridgeDAL/UserManagement/IUserService.cs
@@ -1,8 +1,11 @@
 ﻿namespace IBLLBridgeDAL.UserManagement;
 
-public interface IUserService//用户服务接口
+public interface IUserRegisterService//用户注册服务接口
 {
     void CreateUser(string username, string password);//创建一个新用户
     bool InspectUser(string username);//检测一个用户是否存在
-    bool InspectUser(string username, string password);//检测一个已经存在的用户密码是否正确
+}
+public interface IUserLoginService//用户注册服务接口
+{
+    bool CheckUserPassword(string username,string password);//检测一个用户密码是否正确
 }
