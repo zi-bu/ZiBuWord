0c47329e6d86e787ee69af33bb62e1d496a4f610
Gushiina <1513727898@qq.com>
2025-05-12 13:36:57 +0800

    实现选意页面中

--- 单词背诵程序/UI/MemorizerSelection.cs
+++ 单词背诵程序/UI/MemorizerSelection.cs
@@ -1,8 +1,5 @@
-﻿
-
-
-
-namespace UI;
+﻿namespace UI;
+using static BLL.SelectionOrder;
 
 public partial class MemorizerSelection : Form
 {
@@ -12,33 +9,6 @@ public partial class MemorizerSelection : Form
         this.FormClosing += FormHelper.CloseForm;// 绑定 FormClosing 事件
 
     }
-
-    private void button1_Click(object sender, EventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void checkBox1_CheckedChanged(object sender, EventArgs e)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void button5_Click(object sender, EventArgs e)
-    {
-        //生成测试列表
-
-
-
-
-
-    }
-
-    private void button1_Click_1(object sender, EventArgs e)
-    {
-
-
-    }
-
     private void btnReturn_Click(object sender, EventArgs e)
     {
         HomePage homePage = new HomePage();//创建一个新的主页对象
@@ -47,14 +17,9 @@ public partial class MemorizerSelection : Form
         homePage.Show();//显示主页窗口
         this.Hide();//隐藏当前窗口
     }
-
-    private void button3_Click(object sender, EventArgs e)
+    private void MemorizerSelection_Load(object sender, EventArgs e)//窗体加载时的事件
     {
 
     }
 
-    private void MemorizerSelection_Load(object sender, EventArgs e)
-    {
-
-    }
 }
