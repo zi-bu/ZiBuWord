a10ab0f7a9f0e8bc446706ba0f1db42fbe230d16
鼠标 <2767002082@qq.com>
2025-05-14 16:06:42 +0800

    补点注释

--- 单词背诵程序/DAL/ReturnFunction/ReviewListManagement.cs
+++ 单词背诵程序/DAL/ReturnFunction/ReviewListManagement.cs
@@ -7,13 +7,25 @@ using System.Threading.Tasks;
 using IBLLBridgeDAL;
 namespace DAL.ReturnFunction
 {
+    /// <summary>
+    /// 实现IReviewListManagement接口<br/>
+    /// 本类用于管理复习单词<br/>
+    /// </summary>
     internal class ReviewListManagement: IReviewListManagement
     {
+        /// <summary>
+        /// 获取随机单词进行复习<br/>
+        /// </summary>
+        /// <returns></returns>
         public IWord GetRandomWordForReview()
         {
             Random random = new Random();
             return new Word(random.Next(0,7)) as IWord;
         }
+        /// <summary>
+        /// 添加单词到复习列表<br/>
+        /// </summary>
+        /// <param name="word"></param>
         public void RemoveWordFromReview(IWord word) 
         {
             //先挂着
--- 单词背诵程序/DAL/ReturnFunction/WordManagement.cs
+++ 单词背诵程序/DAL/ReturnFunction/WordManagement.cs
@@ -7,19 +7,33 @@ using System.Text;
 using System.Threading.Tasks;
 namespace DAL
 {
+    /// <summary>
+    /// 实现IWordManagement接口<br/>
+    /// 本类用于管理单词
+    /// </summary>
     internal class WordManagement: IWordManagement
     {
+        /// <summary>
+        /// 获取随机单词的IWord实例
+        /// </summary>
+        /// <returns></returns>
         public IWord GetRandomWordForReciter()
         {
             Random rd = new Random();
             return new Word(rd.Next(0,7)) as IWord;
         }
-
+        /// <summary>
+        /// 移除学习列表中的单词（事实上不需要）
+        /// </summary>
+        /// <param name="word"></param>
         public void RemoveWordFromeLearningList(string word)
         {
             return;
         }
-
+        /// <summary>
+        /// 把单词添加到复习列表中<br/>
+        /// </summary>
+        /// <param name="word"></param>
         public void AddWordToReview(IWord word)
         {
             //未实现
