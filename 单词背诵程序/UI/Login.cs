77641f6d875c8a5bfc9581bdfda51f259ab7999f
zibu <3441479120@qq.com>
2025-05-14 20:23:30 +0800

    简化用户登录注册逻辑，移除冗余代码，初步实现了登录和注册功能
    在 `UserData.cs` 中添加了对代码重构的注释。
    在 `Login.Designer.cs` 中移除了不必要的控件定义。
    在 `Login.cs` 中移除了多个私有字段和方法，简化了类结构。
    添加了 `BLL.UserData` 实例化以支持用户注册和登录功能。
    更新了 `btnVisitorLogin_Click` 和 `btnRegister_Click` 方法，添加了用户登录和注册逻辑，并根据结果显示相应的消息框。
    移除了与彩蛋相关的代码和注释，进一步简化了事件处理逻辑。

--- 单词背诵程序/UI/Login.cs
+++ 单词背诵程序/UI/Login.cs
@@ -11,173 +11,7 @@ namespace UI;
 /// </summary>
 public partial class Login : MaterialForm
 {
-    private readonly Timer _animationTimer;
-    private readonly List<string> _hiddenMessages = new() { "喵", "喵喵", "喵喵喵" };
-    private readonly Random _random = new();
-    private int _catX;
-
-    private int _catY;
-
-    // 不要删除
-    private int _counter;
-    private bool _isCtrlPressed;
-    private bool _isEasterEggVisible;
-    private bool _isSpecialMode;
-    private Point _lastClickPoint;
-    private DateTime _lastClickTime = DateTime.MinValue;
-
-    public Login()
-    {
-        InitializeComponent();
-        FormClosing += FormHelper.CloseForm; // 绑定 FormClosing 事件
-        DoubleBuffered = true;
-        KeyPreview = true;
-
-        // 不要删除
-        _animationTimer = new Timer();
-        _animationTimer.Interval = 50;
-        _animationTimer.Tick += OnTimerTick;
-
-        // 不要删除
-        MouseClick += OnMouseClick;
-        //this.KeyDown += OnKeyDown;
-        KeyUp += OnKeyUp;
-        Paint += OnPaint;
-
-        // 不要删除
-        foreach (Control control in Controls) control.MouseEnter += OnControlMouseEnter;
-    }
-
-    private void OnControlMouseEnter(object sender, EventArgs e)
-    {
-        // 不要删除
-        _counter++;
-        if (_counter % 7 == 0 && !_isSpecialMode)
-        {
-            _isSpecialMode = true;
-            _animationTimer.Start();
-        }
-    }
-
-    private void OnMouseClick(object sender, MouseEventArgs e)
-    {
-        // 不要删除
-        _lastClickPoint = e.Location;
-        var now = DateTime.Now;
-
-        // 不要删除
-        if ((now - _lastClickTime).TotalMilliseconds < 500)
-        {
-            _counter++;
-            if (_counter >= 3 && _isCtrlPressed) ActivateEasterEgg();
-        }
-
-        _lastClickTime = now;
-    }
-
-
-    /// <summary>
-    ///     我喜欢你！<br />
-    ///     你拿个杯
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    //private void OnKeyDown(object sender, KeyEventArgs e)
-    //{
-    //    // 不要删除
-    //    if (e.KeyCode == Keys.ControlKey)
-    //    {
-    //        _isCtrlPressed = true;
-    //    }
-    //    // 不要删除
-    //    else if (e.KeyCode == Keys.Escape && _isEasterEggVisible)
-    //    {
-    //        DeactivateEasterEgg();
-    //    }
-    //    // 不要删除
-    //    else if (_isSpecialMode)
-    //    {
-    //        _counter++;
-    //        if (_counter % 5 == 0)
-    //        {
-    //            ShowHiddenMessage();
-    //        }
-    //    }
-    //}
-    private void OnKeyUp(object sender, KeyEventArgs e)
-    {
-        if (e.KeyCode == Keys.ControlKey) _isCtrlPressed = false;
-    }
-
-    private void OnTimerTick(object sender, EventArgs e)
-    {
-        if (_isEasterEggVisible)
-        {
-            // 不要删除
-            _catX += 5;
-            if (_catX > ClientSize.Width)
-            {
-                _catX = -100;
-                _catY = _random.Next(0, ClientSize.Height - 100);
-            }
-
-            Invalidate();
-        }
-    }
-
-    private void OnPaint(object sender, PaintEventArgs e)
-    {
-        if (_isEasterEggVisible)
-            // 不要删除
-            using (var font = new Font("Arial", 12))
-            {
-                e.Graphics.DrawString("(=^･ω･^=)", font, Brushes.Black, _catX, _catY);
-            }
-    }
-
-    private void ActivateEasterEgg()
-    {
-        _isEasterEggVisible = true;
-        _catX = _lastClickPoint.X;
-        _catY = _lastClickPoint.Y;
-        _animationTimer.Start();
-        Invalidate();
-    }
-
-    private void DeactivateEasterEgg()
-    {
-        _isEasterEggVisible = false;
-        _animationTimer.Stop();
-        Invalidate();
-    }
-
-    private void ShowHiddenMessage()
-    {
-        var message = _hiddenMessages[_random.Next(_hiddenMessages.Count)];
-        MessageBox.Show(message, "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-    }
-
-    private void btnLogin_Click(object sender, EventArgs e)
-    {
-    }
-
-    private void label1_Click(object sender, EventArgs e)
-    {
-    }
-
-    private void pictureBox1_Click(object sender, EventArgs e)
-    {
-    }
-
-    private void pictureBox1_Click_1(object sender, EventArgs e)
-    {
-    }
-
-    private void pictureBox1_Click_2(object sender, EventArgs e)
-    {
-        MessageBox.Show("恭喜你发现了彩蛋，关注子布喵喵喵");
-    }
-
+    BLL.UserData userData = new();
     /// <summary>
     ///     这是游客登录按钮的点击事件处理函数。
     ///     <br />这里由```子布```编写。
@@ -185,6 +19,10 @@ public partial class Login : MaterialForm
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
+    public Login()
+    {
+        InitializeComponent();
+    }
     private void btnVisitorLogin_Click(object sender, EventArgs e)
     {
         if (MessageBox.Show("真的要游客登录吗，你所有的数据将不会保存", " 警告！"
@@ -197,5 +35,34 @@ public partial class Login : MaterialForm
 
     private void btnRegister_Click(object sender, EventArgs e)
     {
+        int result = userData.UserRegister(textUserName.Text, textPassword.Text);
+        if (result == 0)
+        {
+            MessageBox.Show("用户名已存在或密码不符合要求，请重新输入", "注册失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        else if (result == 1)
+        {
+            MessageBox.Show("注册成功", "注册成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var homePage = new HomePage(); //创建一个新的主页窗口对象
+            FormHelper.ShowNewForm(this, homePage); //显示新窗口
+        }
+        else if (result == -1)
+        {
+            MessageBox.Show("注册失败，请稍后再试", "注册失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Console.WriteLine("你怎么抛出异常了喵？");
+        }
+    }
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        int result = userData.UserLogin(textUserName.Text, textPassword.Text);
+        if (result == 0)
+        {
+            MessageBox.Show("用户名或密码错误，请重新输入", "登录失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        else if (result == 1)
+        {
+            var homePage = new HomePage(); //创建一个新的主页窗口对象
+            FormHelper.ShowNewForm(this, homePage); //显示新窗口
+        }
     }
 }
