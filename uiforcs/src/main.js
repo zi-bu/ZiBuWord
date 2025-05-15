4e2e5bd4ca742b4864ce3fefb0d3267fc5a98675
Gushiina <1513727898@qq.com>
2025-05-15 20:33:05 +0800

    导入了bootstrap模块

--- uiforcs/src/main.js
+++ uiforcs/src/main.js
@@ -3,4 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 如需 JS 组件（需依赖 Popper.js）：
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 createApp(App).mount('#app')
