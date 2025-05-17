9fa29d68fe1e49abf1b290b6167911eb5977bd1b
nanhe-7 <1046030056@qq.com>
2025-05-17 11:12:10 +0800

    增加查寻单词功能的框架，功能还待实现

---
+++ 单词背诵程序/UI/SearchWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class SearchWord : Form
+    {
+        public SearchWord()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HomePage homePage = new HomePage(); //创建一个新的主页窗口对象
+            homePage.Show(); //显示新窗口
+            Hide(); //隐藏当前窗口
+        }
+    }
+}
