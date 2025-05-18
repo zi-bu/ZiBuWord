167b7b831ea3b0fff2d37d64f23f1919f40ae666
MingFengOuO <2979397748@qq.com>
2025-05-18 21:03:22 +0800

    添加收藏单词管理类，提供增删查收藏单词的方法

---
+++ 单词背诵程序/DAL/ReturnFunction/FavoriteWordsManagement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Context;
+
+namespace DAL.ReturnFunction
+{
+    /// <summary>
+    /// 收藏单词管理类，提供增删查收藏单词的方法
+    /// </summary>
+    public class FavoriteWordsManagement
+    {
+        /// <summary>
+        /// 添加收藏单词
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        /// <param name="wordId">单词ID</param>
+        /// <param name="dictType">词典类型</param>
+        public void AddFavorite(int userId, int wordId, string dictType)
+        {
+            using var db = new UserContext();
+            // 检查是否已经收藏
+            // 如果没有收藏，则添加
+            if (!db.FavoriteWords.Any(f => f.UserId == userId && f.WordId == wordId && f.DictionaryType == dictType))
+            {
+                db.FavoriteWords.Add(new FavoriteWord
+                {
+                    UserId = userId,
+                    WordId = wordId,
+                    DictionaryType = dictType
+                });
+                db.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// 删除收藏单词
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        /// <param name="wordId">单词ID</param>
+        /// <param name="dictType">词典类型</param>
+        public void RemoveFavorite(int userId, int wordId, string dictType)
+        {
+            using var db = new UserContext();
+            var fav = db.FavoriteWords.FirstOrDefault(f => f.UserId == userId && f.WordId == wordId && f.DictionaryType == dictType);
+            if (fav != null)
+            {
+                db.FavoriteWords.Remove(fav);
+                db.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// 获取用户的收藏单词列表
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        /// <returns>收藏单词列表</returns>
+        public List<FavoriteWord> GetFavorites(int userId)
+        {
+            using var db = new UserContext();
+            return db.FavoriteWords.Where(f => f.UserId == userId).ToList();
+        }
+    }
+}
