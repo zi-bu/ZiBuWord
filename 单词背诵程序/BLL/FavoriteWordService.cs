f06611f06e6fbc30326d4c4375ba6c5abbd1ba63
MingFengOuO <2979397748@qq.com>
2025-05-22 16:50:56 +0800

    修复了串层的问题

--- 单词背诵程序/BLL/FavoriteWordService.cs
+++ 单词背诵程序/BLL/FavoriteWordService.cs
@@ -14,6 +14,7 @@ namespace BLL
     public class FavoriteWordService
     {
         private readonly FavoriteWordsManagement _dal = new FavoriteWordsManagement();
+        private readonly FavoriteWordDetailQueryDAL _detailDal = new FavoriteWordDetailQueryDAL();
 
         /// <summary>
         /// 添加收藏
@@ -41,100 +42,23 @@ namespace BLL
         {
             var favorites = _dal.GetFavorites(userId);
             var result = new List<FavoriteWordDetail>();
-            using var db = new DAL.Context.SqlDataContext();
             foreach (var fav in favorites)
             {
-                string word = "";
-                string translation = "";
-
-                // 1. 先查出单词原文
-                switch (fav.DictionaryType)
-                {
-                    case "CET4":
-                        var cet4 = db.CET4.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (cet4 != null)
-                        {
-                            word = cet4.Word;
-                        }
-                        break;
-                    case "CET6":
-                        var cet6 = db.CET6.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (cet6 != null)
-                        {
-                            word = cet6.Word;
-                        }
-                        break;
-                    case "HighSchool":
-                        var hs = db.HighSchool.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (hs != null)
-                        {
-                            word = hs.Word;
-                        }
-                        break;
-                    case "MiddleSchool":
-                        var ms = db.MiddleSchool.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (ms != null)
-                        {
-                            word = ms.Word;
-                        }
-                        break;
-                    case "KY":
-                        var ky = db.KY.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (ky != null)
-                        {
-                            word = ky.Word;
-                        }
-                        break;
-                    case "TF":
-                        var tf = db.TF.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (tf != null)
-                        {
-                            word = tf.Word;
-                        }
-                        break;
-                    case "SAT":
-                        var sat = db.SAT.FirstOrDefault(w => w.Id == fav.WordId);
-                        if (sat != null)
-                        {
-                            word = sat.Word;
-                        }
-                        break;
-                }
-
-                // 2. 获取 Formid
-                Formid formid = fav.DictionaryType switch
-                {
-                    "CET4" => Formid.CET4,
-                    "CET6" => Formid.CET6,
-                    "HighSchool" => Formid.HighSchool,
-                    "MiddleSchool" => Formid.MiddleSchool,
-                    "KY" => Formid.KY,
-                    "TF" => Formid.TF,
-                    _ => throw new Exception("未知词典类型")
-                };
-
-                // 3. 用 Word 类查释义和词性
-                string pos = "";
-                if (!string.IsNullOrEmpty(word))
-                {
-                    var wordObj = new DAL.Word(word, formid);
-                    var pairs = wordObj.translations.Zip(wordObj.pos, (tran, p) => $"{p}. {tran}");
-                    translation = string.Join("; ", pairs);
-                }
-
+                var (word, posList, translationsList) = _detailDal.GetWordDetail(fav.DictionaryType, fav.WordId);
+                var translation = string.Join("; ", translationsList.Zip(posList, (tran, p) => $"{p}. {tran}"));
                 result.Add(new FavoriteWordDetail
                 {
                     Id = fav.Id,
                     DictionaryType = fav.DictionaryType,
                     WordId = fav.WordId,
                     Word = word,
-                    Translation = translation,
-                    Pos = pos
+                    Translation = translation
                 });
             }
             return result;
         }
     }
+
     public class FavoriteWordDetail
     {
         public int Id { get; set; }
@@ -142,6 +66,6 @@ namespace BLL
         public int WordId { get; set; }
         public string? Word { get; set; }
         public string? Translation { get; set; }
-        public string? Pos { get; set; } 
+        public string? Pos { get; set; }
     }
 }
---
+++ 单词背诵程序/DAL/FavoriteWordDetailQueryDAL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DAL.Context;
+
+namespace DAL
+{
+    public class FavoriteWordDetailQueryDAL
+    {
+        /// <summary>
+        /// 根据词典类型和单词ID获取单词原文、词性、释义
+        /// </summary>
+        public (string Word, List<string> Pos, List<string> Translations) GetWordDetail(string dictType, int wordId)
+        {
+            using var db = new SqlDataContext();
+            switch (dictType)
+            {
+                case "CET4":
+                    var cet4 = db.CET4.FirstOrDefault(w => w.Id == wordId);
+                    if (cet4 != null)
+                    {
+                        var translations = cet4.Translations.Select(t => t.Translation).ToList();
+                        var pos = cet4.Translations.Select(t => t.TyPe).ToList();
+                        return (cet4.Word, pos, translations);
+                    }
+                    break;
+                case "CET6":
+                    var cet6 = db.CET6.FirstOrDefault(w => w.Id == wordId);
+                    if (cet6 != null)
+                    {
+                        var translations = cet6.Translations.Select(t => t.Translation).ToList();
+                        var pos = cet6.Translations.Select(t => t.TyPe).ToList();
+                        return (cet6.Word, pos, translations);
+                    }
+                    break;
+                case "HighSchool":
+                    var hs = db.HighSchool.FirstOrDefault(w => w.Id == wordId);
+                    if (hs != null)
+                    {
+                        var translations = hs.Translations.Select(t => t.Translation).ToList();
+                        var pos = hs.Translations.Select(t => t.TyPe).ToList();
+                        return (hs.Word, pos, translations);
+                    }
+                    break;
+                case "MiddleSchool":
+                    var ms = db.MiddleSchool.FirstOrDefault(w => w.Id == wordId);
+                    if (ms != null)
+                    {
+                        var translations = ms.Translations.Select(t => t.Translation).ToList();
+                        var pos = ms.Translations.Select(t => t.TyPe).ToList();
+                        return (ms.Word, pos, translations);
+                    }
+                    break;
+                case "KY":
+                    var ky = db.KY.FirstOrDefault(w => w.Id == wordId);
+                    if (ky != null)
+                    {
+                        var translations = ky.Translations.Select(t => t.Translation).ToList();
+                        var pos = ky.Translations.Select(t => t.TyPe).ToList();
+                        return (ky.Word, pos, translations);
+                    }
+                    break;
+                case "TF":
+                    var tf = db.TF.FirstOrDefault(w => w.Id == wordId);
+                    if (tf != null)
+                    {
+                        var translations = tf.Translations.Select(t => t.Translation).ToList();
+                        var pos = tf.Translations.Select(t => t.TyPe).ToList();
+                        return (tf.Word, pos, translations);
+                    }
+                    break;
+                case "SAT":
+                    var sat = db.SAT.FirstOrDefault(w => w.Id == wordId);
+                    if (sat != null)
+                    {
+                        var translations = sat.Translations.Select(t => t.Translation).ToList();
+                        var pos = sat.Translations.Select(t => t.TyPe).ToList();
+                        return (sat.Word, pos, translations);
+                    }
+                    break;
+            }
+            return ("", new List<string>(), new List<string>());
+        }
+    }
+}
