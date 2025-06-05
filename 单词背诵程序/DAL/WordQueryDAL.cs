c571019cee7c9a0e5057bb633887e793a50e5c0c
nanhe-7 <1046030056@qq.com>
2025-06-05 20:16:04 +0800

    再优化一下

--- 单词背诵程序/DAL/WordQueryDAL.cs
+++ 单词背诵程序/DAL/WordQueryDAL.cs
@@ -11,13 +11,14 @@ namespace DAL
             foreach (Formid formid in Enum.GetValues(typeof(Formid)))
             {
                 var results = WordMover.FindWordsByEnglish(input, formid);
+                if (results.Count == 1 && results[0].Equals(input, StringComparison.OrdinalIgnoreCase))
+                    break;
                 foreach (var word in results)
                 {
                     list.Add(new Word(word, formid));
                 }
                 // 如果当前表有精确匹配，WordMover会直接返回单个结果，这里直接break即可
-                if (results.Count == 1 && results[0].Equals(input, StringComparison.OrdinalIgnoreCase))
-                    break;
+                
             }
             // 去重
             return list.GroupBy(w => w.word).Select(g => g.First()).ToList();
@@ -29,13 +30,14 @@ namespace DAL
             foreach (Formid formid in Enum.GetValues(typeof(Formid)))
             {
                 var results = WordMover.FindWordsByChinese(input, formid);
+                if (results.Count == 1)
+                    break;
                 foreach (var word in results)
                 {
                     list.Add(new Word(word, formid));
                 }
                 // 如果当前表有精确匹配，WordMover会直接返回单个结果，这里直接break即可
-                if (results.Count == 1)
-                    break;
+                
             }
             // 去重
             return list.GroupBy(w => w.word).Select(g => g.First()).ToList();
